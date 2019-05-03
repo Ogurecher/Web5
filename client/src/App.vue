613fbc0fdffbb4281b14324a0eae15c68bf3385d
Dmitrii <1@1.1>
2019-05-04 02:52:00 +0300

    fin

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
     <router-view/>
-    <div id="output-textarea">
-        <p><span v-html="marked_message"></span></p>
+    <div id="list">
+        <button v-on:click.prevent='display'>Display</button>
+        <ul>
+          <li v-for="doc in docs">{{ doc.name }}
+            <button v-on:click.prevent='edit_start(doc._id)'>Edit</button>
+            <button v-on:click.prevent="del(doc._id)">Delete</button>
+          </li>
+        </ul>
+    </div>
+    <div id="editor">
         <form>
-          <textarea v-model="message"></textarea>
-          <button v-on:click.prevent='save'> Save</button>
+          <input id="input-name" v-model="name">
+          <textarea id="input-content" v-model="content"></textarea>
+          <button v-if="editing_id == ''" v-on:click.prevent='save'> Save</button>
+          <button v-else v-on:click.prevent='edit'> Edit</button>
         </form>
+    </div>
+    <div id="preview">
+        <span v-html="marked_content"></span>
     </div>
   </div>
 </template>
@@ -18,20 +31,62 @@ export default {
   name: 'App',
   data () {
     return {
-      message: 'Vue!'
+      content: 'content',
+      name: 'name',
+      docs: [],
+      editing_id: ''
     }
   },
   computed: {
-    marked_message: function () {
-      return xss(marked(this.message))
+    marked_content: function () {
+      return xss(marked(this.content))
     }
   },
   methods: {
     save: function () {
-      this.$http.post('http://localhost:8081', {
-        message: this.message
+      this.$http.post('http://localhost:8081/save', {
+        name: this.name,
+        content: this.content
       }).then(function (data) {
-        console.log(data)
+        this.display();
+      })
+    },
+
+    display: function () {
+      this.$http.get('http://localhost:8081/display', {
+      }).then(function (data) {
+        this.docs = data.data;
+        console.log(data);
+      })
+    },
+
+    del: function (doc_id) {
+      this.$http.post('http://localhost:8081/del', {
+        id: doc_id
+      }).then(function (data) {
+        this.display();
+      })
+    },
+
+    edit_start: function (doc_id) {
+      const doc = this.docs.find(function (element, index, arr) {
+        return element._id == doc_id;
+      });
+      this.name = doc.name;
+      this.content = doc.content;
+      this.editing_id = doc_id;
+    },
+
+    edit: function () {
+      this.$http.post('http://localhost:8081/edit', {
+        id: this.editing_id,
+        name: this.name,
+        content: this.content
+      }).then(function (data) {
+        this.editing_id = '';
+        this.name = 'name';
+        this.content = 'content';
+        this.display();
       })
     }
   }
@@ -39,12 +94,47 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0 auto;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  #list {
+    float: left;
+    width: 250px;
+    margin-top: 3%;
+    margin-right: 2%;
+    background: #ececec;
+  }
+
+  #editor {
+    float: left;
+    width: 35%;
+    margin-right: 2%;
+    height: 500px;
+    padding: 10px;
+    background: #ececec;
+  }
+
+  #preview {
+    float: left;
+    width: 30%;
+    height: 500px;
+    padding: 10px;
+    background: #ececec;
+  }
+
+  #input-name {
+    width: 100%;
+    margin-bottom: 8px;
+  }
+
+  #input-content {
+    width: 100%;
+    height: 440px;
+  }
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -2,32 +2,80 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const mongodb = require('mongodb')
+const objectId = require('mongodb').ObjectID;
 const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-const url = 'mongodb://localhost:27017/';
+const url = 'mongodb://localhost:27017/Markdown';
 
-app.post('/', function (req, res) {
-    res.send(req.body);
-    MongoClient.connect(url, function(err, client) {
-      var db = client.db('Markdown');
-      if (err) throw err;
-      console.log("Switched to "+db.databaseName+" database");
-      db.collection("documents").insertOne(req.body, function(err, res) {
-          if (err) throw err;
-          console.log("Document inserted");
-          client.close();
-      });
+app.post('/save', function (req, res) {
+  const doc = {
+    name: req.body.name,
+    content: req.body.content
+  };
+  res.send(req.body);
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('documents').insertOne(doc, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item ' + doc.name + ' inserted');
+      db.close();
     });
+  });
 });
 
 app.get('/', function (req, res) {
   res.send('Hello World, you got /');
+});
+
+app.get('/display', function (req, res) {
+  const docArray = [];
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    const cursor = db.collection('documents').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      docArray.push(doc);
+    }, function() {
+      db.close();
+      res.send(docArray);
+    });
+  });
+});
+
+app.post('/del', function (req, res) {
+  const id = req.body.id
+  res.send(req.body.id);
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('documents').deleteOne({"_id": objectId(id) }, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item ' + id + ' deleted');
+      db.close();
+    });
+  });
+});
+
+app.post('/edit', function (req, res) {
+  const doc = {
+    name: req.body.name,
+    content: req.body.content
+  };
+  const id = req.body.id
+  res.send(req.body.id);
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('documents').updateOne({"_id": objectId(id) }, {$set: doc}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item ' + id + ' edited');
+      db.close();
+    });
+  });
 });
 
 app.listen(process.env.PORT || 8081)
