4e89ecee2de817de6fc1bf2c32d51655f0b0558b
Dmitrii <1@1.1>
2019-05-03 15:09:14 +0300

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <div id="output-textarea">
+        <p><span v-html="marked_message"></span></p>
+        <input v-model="message">
+    </div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+const xss = require('xss')
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'Vue!'
+    }
+  },
+  computed: {
+    marked_message: function () {
+      return xss(marked(this.message))
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Web5.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Web5">
+    <div id="output-textarea">
+      <p>{{ message }}</p>
+      <input v-model="message">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Web5',
+  data () {
+    return {
+      message: 'Vue!'
+    }
+  }/* ,
+  computed: {
+    marked_message: function () {
+      return marked(this.message)
+    }
+  } */
+}
+</script>
